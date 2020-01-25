23007dd2c2ad7cc302f50b81f340cdb4b035446d
susannaaz <susanna.azzoni@gmail.com>
2020-01-25 13:50:56 +0000

    improved

--- beta_sync.py
+++ beta_sync.py
@@ -12,36 +12,60 @@ ells = np.arange(lmax+1)
 
 # Power law model
 #---------------------------------------------------
-cl_betaSync= ((ells+0.001) / 80.)**gamma_beta #  # A_beta *                                 
+
+# Gamma (convergence)
+gamma_beta_sync = -2.5
+gamma_beta_dust = -3.5
+
+# Calculate power spectrum (input)                                             #---------------------------------------------------                           
+def powerlaw(ells, gamma):
+    c_ells = ((ells+0.001) / 80.)**gamma
+    return c_ells
+
+cl_betaSync = powerlaw(ells, gamma_beta_sync)
+cl_betaDust = powerlaw(ells, gamma_beta_dust)
 
 # Map from given power spectrum as mean + gaussian random field
 #---------------------------------------------------
-delta_map = hp.synfast(cl_betaSync, nside, new=True, verbose=False)
+def map_beta(ells, gamma):
+    cl_beta = powerlaw(ells, gamma)
+    delta_map = hp.synfast(cl_beta, nside, new=True, verbose=False)
+    sigma_map = np.sqrt(np.mean(delta_map**2))
+    sigma_des = 0.1 #desired typical map variation
+    delta_beta = delta_map * sigma_des / sigma_map
+    return delta_beta + np.mean(delta_beta)
 
-sigma_map = np.sqrt(np.mean(delta_map**2))
-sigma_des = 0.1 #desired typical map variation                                                                             
-delta_beta = delta_map * sigma_des / sigma_map
+map_beta_sync = map_beta(ells, gamma_beta_sync)
+map_beta_dust = map_beta(ells, gamma_beta_dust)
 
-map_beta = delta_beta + np.mean(delta_beta)
-
-hp.mollview(map_beta, unit='$\\beta$',  title='Scaling Index beta_sync') #unit='$\\beta$'                                  
-delta_map -= delta_beta + np.mean(delta_beta)
-map_beta1 = delta_map
-
-hp.mollview(map_beta1, unit='$\\beta$',  title='Scaling Index beta_sync') #unit='$\\beta$'                                 
+hp.mollview(map_beta_sync, title='Scaling Index beta_sync') #unit='$\\beta$'
+hp.mollview(map_beta_dust, title='Scaling Index beta_dust') #unit='$\\beta$'                              
 plt.show()
+
+map_beta_sync_fin = hp.synfast(cl_betaSync, nside, new=True, verbose=False) - map_beta_sync
+map_beta_dust_fin = hp.synfast(cl_betaDust, nside, new=True, verbose=False) - map_beta_dust
 
 # Power spectrum from map (output)
 #---------------------------------------------------
-cl_betaSync_out = hp.anafast(map_beta, lmax=lmax)
-cl_betaSync_out1 = hp.anafast(map_beta1, lmax=lmax)
+cl_betaSync_out = hp.anafast(map_beta_sync_fin, lmax=lmax)
+cl_betaDust_out = hp.anafast(map_beta_dust_fin, lmax=lmax)
 
 # Compare input and output power spectra 
 #---------------------------------------------------
 plt.figure()
-plt.semilogy(ells, cl_betaSync, 'b',label="input power spectrum")
-#plt.semilogy(ells, cl_betaSync_out, 'r',label="output power spectrum")
-plt.semilogy(ells, cl_betaSync_out1, 'r',label="output power spectrum 1")
+plt.semilogy(ells, cl_betaSync, 'b',label="input power spectrum sync")
+plt.semilogy(ells, cl_betaSync_out, 'r',label="output power spectrum sync")
+#plt.semilogy(ells, cl_betaSync_out1, 'r',label="output power spectrum 1")                                
+plt.xlabel('$\ell$')
+plt.ylabel('$C_\ell$')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.figure()
+plt.semilogy(ells, cl_betaDust, 'b',label="input power spectrum dust")
+plt.semilogy(ells, cl_betaDust_out, 'r',label="output power spectrum dust")
+#plt.semilogy(ells, cl_betaSync_out1, 'r',label="output power spectrum 1")                                
 plt.xlabel('$\ell$')
 plt.ylabel('$C_\ell$')
 plt.legend()
